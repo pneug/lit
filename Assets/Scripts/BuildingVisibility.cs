638514cb786bcf5447971a418e76788b65279578
Philip <philngbr@googlemail.com>
2022-09-17 15:22:02 +0200

    Improved visibility settings

--- Assets/Scripts/BuildingVisibility.cs
+++ Assets/Scripts/BuildingVisibility.cs
@@ -5,32 +5,65 @@ using UnityEngine;
 public class BuildingVisibility : MonoBehaviour
 {
     public GameObject[] BuildingParts;
-    private int currVisibility;
+    private int currTransparentLv;
     
     void Start()
     {
-        currVisibility = BuildingParts.Length - 1;
-        for (int i = 0; i < BuildingParts.Length; i++)
+        currTransparentLv = BuildingParts.Length - 1;
+        for (int i = 0; i < BuildingParts.Length - 1; i++)
         {
-            BuildingParts[i].SetActive(i < currVisibility);
+            ChangeVisibility(currTransparentLv, Visibility.opaque);
+            
+        }
+        ChangeVisibility(currTransparentLv, Visibility.transparent);
+    }
+
+    private void ChangeVisibility(int floor, Visibility visibility)
+    {
+        if (floor < 0 || floor >= BuildingParts.Length)
+        {
+            return;
+        }
+        
+        Renderer[] renderers = BuildingParts[floor].GetComponentsInChildren<Renderer>();
+        float transparency = 0;
+        if (visibility == Visibility.opaque)
+        {
+            transparency = 1;
+        }
+        else if (visibility == Visibility.transparent)
+        {
+            transparency = 0.3f;
+        }
+        foreach (Renderer renderer in renderers)
+        {
+            renderer.material.color = new Color(renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, transparency);
         }
     }
 
     void Update()
     {
-        if (OVRInput.GetDown(OVRInput.Button.One))
+        if (OVRInput.GetDown(OVRInput.Button.Three) || Input.GetKeyDown(KeyCode.DownArrow))
         {
-            if (currVisibility > 0)
+            if (currTransparentLv > 0)
             {
-                BuildingParts[--currVisibility].SetActive(false);
+                currTransparentLv--;
             }
         }
-        if (OVRInput.GetDown(OVRInput.Button.Two))
+        if (OVRInput.GetDown(OVRInput.Button.Four) || Input.GetKeyDown(KeyCode.UpArrow))
         {
-            if (currVisibility < BuildingParts.Length)
+            if (currTransparentLv < BuildingParts.Length)
             {
-                BuildingParts[currVisibility++].SetActive(true);
+                currTransparentLv++;
             }
         }
+        //ChangeVisibility(currTransparentLv + 1, Visibility.invisible);
+        ChangeVisibility(currTransparentLv, Visibility.transparent);
+        ChangeVisibility(currTransparentLv - 1, Visibility.opaque);
     }
+}
+
+public enum Visibility
+{
+    invisible, transparent, opaque
 }
